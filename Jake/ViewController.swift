8e86c68f561ad4242685edf86850e28979716a20
Anderson Kloss Maia <anderson.kloss@gmail.com>
2018-09-18 22:04:09 -0300

    Jake

---
+++ Jake/ViewController.swift
@@ -0,0 +1,106 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var arms: UIImageView!
+    @IBOutlet weak var ears: UIImageView!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBOutlet weak var iris: UIImageView!
+    @IBOutlet weak var nose: UIImageView!
+    @IBOutlet weak var pizza: UIImageView!
+    
+    private var animator: UIDynamicAnimator!
+    private var snapping: UISnapBehavior!
+    
+    private var fixedCenterByPizza: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        snapping = UISnapBehavior(item: pizza, snapTo: view.center)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pannedPizza))
+        pizza.addGestureRecognizer(panGesture)
+        pizza.isUserInteractionEnabled = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        fixedCenterByPizza = pizza.center
+    }
+
+    @objc func pannedPizza(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            animator.removeBehavior(snapping)
+        case .changed:
+            let translation = recognizer.translation(in: view)
+            
+            let point = CGPoint(x: pizza.center.x + translation.x,
+                                y: pizza.center.y + translation.y)
+            
+            pizza.center = point
+            animateEyes(by: point)
+            animateIris(by: point)
+            animateNose(by: point)
+            animateArms(by: point)
+            animateEars(by: point)
+            
+            recognizer.setTranslation(.zero, in: view)
+        case .ended, .cancelled, .failed:
+            animator.addBehavior(snapping)
+            
+            UIView.animate(withDuration: TimeInterval(snapping.damping)) {
+                self.eyes.transform = .identity
+                self.iris.transform = .identity
+                self.nose.transform = .identity
+                self.arms.transform = .identity
+                self.ears.transform = .identity
+            }
+        case .possible:
+            break
+        }
+    }
+    
+    private func animateEyes(by point: CGPoint) {
+        let x = (point.x - fixedCenterByPizza.x) * 0.1
+        let y = (point.y - fixedCenterByPizza.y) * 0.1
+        
+        eyes.transform = CGAffineTransform(translationX: max(min(x * 0.5 , 10.0), -10.0),
+                                           y: max(min(y, 10.0), -10.0))
+    }
+    
+    private func animateIris(by point: CGPoint) {
+        let x = (point.x - fixedCenterByPizza.x) * 0.1
+        let y = (point.y - fixedCenterByPizza.y) * 0.1
+        
+        iris.transform = CGAffineTransform(translationX: max(min(x, 16.0), -16.0),
+                                           y: max(min(y, 16.0), -16.0))
+    }
+    
+    private func animateNose(by point: CGPoint) {
+        let x = (point.x - fixedCenterByPizza.x) * 0.1
+        let y = (point.y - fixedCenterByPizza.y) * 0.1
+        
+        nose.transform = CGAffineTransform(translationX: max(min(x, 10.0), -10.0),
+                                           y: max(min(y, 10.0), -10.0))
+    }
+    
+    private func animateArms(by point: CGPoint) {
+        let x = (point.x - fixedCenterByPizza.x) * 0.1
+        let y = (point.y - fixedCenterByPizza.y) * 0.1
+        
+        arms.transform = CGAffineTransform(translationX: -max(min(x, 8.0), -8.0),
+                                           y: -max(min(y, 8.0), -8.0))
+    }
+    
+    private func animateEars(by point: CGPoint) {
+        let x = (point.x - fixedCenterByPizza.x) * 0.1
+        let y = (point.y - fixedCenterByPizza.y) * 0.1
+        
+        ears.transform = CGAffineTransform(translationX: -max(min(x, 6.0), -6.0),
+                                           y: -max(min(y, 6.0), -6.0))
+    }
+}
+
